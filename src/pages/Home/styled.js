abde34798c45ebd1783cfc19407d192f468905fc
Ana Luíza <analuizareism@hotmail.com>
2021-05-20 15:36:03 -0300

    Responsividade pagina Home

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const mobileSize = "720px"
+
 export const Header = styled.header`
   width: 100%;
   height: 100px;
@@ -9,6 +11,10 @@ export const Header = styled.header`
   align-items: center;
   justify-content: flex-end;
   padding: 0 10%;
+
+  @media (max-width: ${mobileSize}) {
+    height: 60px;
+  }
 `;
 
 export const StyledLink = styled(Link)`
@@ -25,11 +31,20 @@ export const Hero = styled.section`
   display: flex;
   align-items: flex-end;
   justify-content: space-between;
+
+  @media (max-width: ${mobileSize}) {
+    flex-direction: column;
+    align-items: center;
+  } 
 `;
 
 export const Title = styled.h1`
   font-size: 72px;
   font-weight: 400;
+
+  @media (max-width: ${mobileSize}) {
+    font-size: 42px;
+  }   
 `;
 
 export const Text = styled.p`
@@ -54,6 +69,13 @@ export const Button = styled.button`
   background-color: #c4c4c4;
   border-radius: 24px;
   margin-left: 12px;
+
+  @media (max-width: ${mobileSize}) {
+    display: block;
+    margin-top: 20px;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 export const SectionText = styled.section`
@@ -61,10 +83,18 @@ export const SectionText = styled.section`
   height: 602px;
   background-color: #c4c4c4;
   padding: 10%;
+
+  @media (max-width: 720px) {
+    height: auto;
+  }
 `;
 
 export const ContentWrapper = styled.div`
   width: 40%;
+
+  @media (max-width: ${mobileSize}) {
+    width: 100%;
+  } 
 `;
 
 export const Description = styled.p`
@@ -76,6 +106,11 @@ export const SectionComments = styled.section`
   height: 1000px;
   background-color: #e5e5e5;
   padding: 120px 10%;
+
+  @media (max-width: ${mobileSize}) {
+    height: auto;
+    width: 100%;
+  }
 `;
 
 export const CardComments = styled.div`
@@ -93,6 +128,10 @@ export const PositionCard = styled.div`
   flex-wrap: wrap;
   width: 740px;
   margin: 120px auto;
+
+  @media (max-width: ${mobileSize}) {
+    width: 100%;
+  }
 `;
 
 export const Footer = styled.footer`
